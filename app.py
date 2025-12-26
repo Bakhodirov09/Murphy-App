8e51692107642deeddb63b7dfa5c85b1bcf7da91
Bakhodirov09 <bloghash01@gmail.com>
2025-12-26 14:21:53 +0500

    app.py updated

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.dispatcher import FSMContext
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from data import BOT_TOKEN
 
@@ -10,7 +9,10 @@ async def link_btn(chat_id):
     link_button = InlineKeyboardMarkup(
         inline_keyboard=[
             [
-                InlineKeyboardButton(text='Murphy-Homework Web-Site', web_app=WebAppInfo(url=f'https://12b3492d0594.ngrok-free.app?chat_id{chat_id}'))
+                InlineKeyboardButton(
+                    text='Murphy-Homework Web-Site',
+                    web_app=WebAppInfo(url=f'https://12b3492d0594.ngrok-free.app?chat_id={chat_id}')
+                )
             ]
         ]
     )
@@ -18,15 +20,14 @@ async def link_btn(chat_id):
 
 @dp.message_handler(commands='start')
 async def start_handler(message: types.Message):
-    # text = "We are sorry, but we are adding questions to the Murphy-App for students, and working on its security. 🛡\nMurphy-App for students is ready: 87.5%."
+    username = f"(@{message.from_user.username})" if message.from_user.username else ""
     text = f"""
-👋 Hello, Dear: <b>{message.from_user.full_name}</b> {f'({message.from_user.username})' if message.from_user.username.startswith('@') else f'(@{message.from_user.username})' if message.from_user.username else ''}.
+👋 Hello, Dear: <b>{message.from_user.full_name}</b> {username}.
 Welcome to our bot. Please click 'Murphy-Homework Web-Site' button, and start doing your Murphy-Homeworks.
 
 ✊ GOOD LUCK...!
 """
-    await message.answer(text=text, reply_markup=await link_btn(message.chat.id), protect_content=True, parse_mode='HTML')
-
+    await message.answer(text=text, reply_markup=await link_btn(message.chat.id), parse_mode='HTML')
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
