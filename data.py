35f4ca822b24aea47bac9be69cd17ca9ee3dc7d5
Bakhodirov09 <bloghash01@gmail.com>
2026-01-03 20:11:54 +0500

    Errors fixed

--- data.py
+++ data.py
@@ -1,20 +1,3 @@
-from web.models import EssentialWordsModel
-from web.database import SessionLocal
+from web.data import env
 
-def capitalize_words():
-    db = SessionLocal()
-    try:
-        words = db.query(EssentialWordsModel).all()
-        for word in words:
-            word.translation_uz = word.translation_uz.capitalize()
-            word.translation_ru = word.translation_ru.capitalize()
-
-        db.commit()
-    except Exception as e:
-        db.rollback()
-        print(e)
-    finally:
-        print('✅ Done')
-        db.close()
-
-capitalize_words()
+BOT_TOKEN = env.str('BOT_TOKEN')
