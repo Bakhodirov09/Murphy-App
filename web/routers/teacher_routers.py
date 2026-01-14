bf1215efc351f671552a669e6d26bc99e9248e1e
Bakhodirov09 <bloghash01@gmail.com>
2026-01-14 14:19:42 +0500

    Getting student results function added for teacher dashboard

--- web/routers/teacher_routers.py
+++ web/routers/teacher_routers.py
@@ -420,7 +420,7 @@ async def get_results(db: db_dependency, student_id: UUID = Query(...), week_id:
     if type == 'Vocab':
         query = query.options(selectinload(StudentResultsModel.vocabulary))
     elif type == 'Exercise':
-        query = query.options(selectinload(StudentResultsModel.exercise.id))
+        query = query.options(selectinload(StudentResultsModel.exercise))
 
     results = query.all()
     return {'ok': True, 'results': results}
