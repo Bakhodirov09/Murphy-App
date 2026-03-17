6d24ed74951dd496a1785ff376e2a9829d6e6b8a
Bakhodirov09 <bloghash01@gmail.com>
2026-03-17 15:07:25 +0500

    Errors fixed

--- web/routers/teacher_routers.py
+++ web/routers/teacher_routers.py
@@ -524,7 +524,9 @@ async def get_results(
         }
 
     elif type == 'Exercise':
+        print(week.book)
         book = db.query(BooksModel).filter(BooksModel.id == week.book).first()
+        print(book)
         is_ielts = book.level == LevelsEnum.IELTS
 
         if is_ielts:
